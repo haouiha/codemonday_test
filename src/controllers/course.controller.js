cb37c7821884a6d7eb2c246339c162e95fae2b13
OuiAtichat <haouiha@hotmail.com>
2021-05-04 14:14:34 +0700

    update courses routes

--- src/controllers/course.controller.js
+++ src/controllers/course.controller.js
@@ -1,9 +1,33 @@
 import { Course, Teacher } from '../database/models';
 
 export const getAllCourses = async (req, res) => {
-	res.send('ok');
+	try {
+		const allCourses = await Course.findAll({ include: 'onwer' });
+		res.send(allCourses);
+	} catch (error) {
+		console.log(`error`, error);
+		res.status(500).send({ error: 'error' });
+	}
 };
 
 export const createCourse = async (req, res) => {
-	res.send('ok');
+	const { course_name, course_description, onwer_id } = req.body;
+	try {
+		const courseTeacher = await Teacher.findOne({ where: { id: onwer_id } });
+		console.log(`courseTeacher`, courseTeacher);
+		if (!courseTeacher) throw { code: 400, error: new Error('invalid onwer_id') };
+
+		const newCourse = await Course.create({
+			course_name,
+			course_description,
+			onwer_id,
+		});
+
+		res.send(newCourse);
+	} catch (error) {
+		if (error.code) {
+			return res.status(error.code).send({ error: error.error.message || error });
+		}
+		res.status(500).send({ error: error.message || error });
+	}
 };
